f90bd91dba41a1b06b963d7ebbe32fbef7f18413
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-04-25 13:47:04 +0700

    Cyduc headline dengan tanggal

--- src/main/java/com/pacilkom/feats/scele/latest-news/NewsScrapper.java
+++ src/main/java/com/pacilkom/feats/scele/latest-news/NewsScrapper.java
@@ -4,23 +4,23 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class NewsScrapper {
 
     public static final String SCELE_LINK = "https://scele.cs.ui.ac.id/";
 
-    public static String getNews() throws IOException {
+    public static List<String> getNews() throws IOException {
         Document doc = Jsoup.connect(SCELE_LINK).get();
-        String output = "";
+        List<String> messages = new LinkedList<>();
         Elements block = doc.select("div#site-news-forum");
         for (Element el : block.select("div.forumpost")) {
-            output += el.attr("aria-label")+"\n";
+            Elements text = el.select("div.topic");
+            String title = text.select("div.subject").text();
+            String author = text.select("div.author").text();
+            messages.add(title + " " + author + "\n");
         }
-        return output;
+        return messages;
     }
-
-    public static void main (String[] args) throws IOException {
-        System.out.println(getNews());
-    }
-
 }
