97294988ab50269553599c71421825c0f308bf25
Dennis Febri Dien <dennis.febri@ui.ac.id>
2018-04-27 18:37:54 +0700

    change getTimewithScrapping with getTimewithNtpudpClient

--- src/main/java/com/pacilkom/feats/scele/latestTime/TimeScrapper.java
+++ src/main/java/com/pacilkom/feats/scele/latestTime/TimeScrapper.java
@@ -1,15 +1,29 @@
 package com.pacilkom.feats.scele.latestTime;
 
 import com.pacilkom.feats.scele.Scrapper;
+import java.net.InetAddress;
+import java.util.Date;
+
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+
+
 
 public class TimeScrapper {
 
-    private static final String IDSERVERTIME = "input#block_progress_serverTime";
+    private static final String TIME_SERVER = "ntp.ui.ac.id";  
 
     public static String getTime() {
-        return Scrapper
-                .getInstance()
-                .getDocument()
-                .selectFirst(IDSERVERTIME).text();
+        return timeLookup();
+    }
+    
+    private static String timeLookup() {
+         
+        NTPUDPClient timeClient = new NTPUDPClient();
+        InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
+        TimeInfo timeInfo = timeClient.getTime(inetAddress);
+        long returnTime = timeInfo.getReturnTime();
+        Date time = new Date(returnTime);
+        return "Time from " + TIME_SERVER + ": " ;//+ time;
     }
 }
