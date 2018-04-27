8b8471e03ecc1608da01c20cfe1b828ad60e3de0
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-04-27 20:23:02 +0700

    Nyoba parse mode markdown yg bener

--- src/main/java/com/pacilkom/feats/scele/latestTime/SceleTimeCommand.java
+++ src/main/java/com/pacilkom/feats/scele/latestTime/SceleTimeCommand.java
@@ -1,13 +1,14 @@
 package com.pacilkom.feats.scele.latestTime;
 
 import com.pacilkom.feats.BotCommand;
-import org.telegram.telegrambots.api.methods.ParseMode;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 
 public class SceleTimeCommand implements BotCommand {
 
     @Override
     public SendMessage execute(Long chatId) throws Exception {
-        return new SendMessage(chatId, TimeScrapper.getTime(), ParseMode.MARKDOWN);
+        SendMessage reply = new SendMessage(chatId, TimeScrapper.getTime());
+        reply.enableMarkdown(true);
+        return reply;
     }
 }
