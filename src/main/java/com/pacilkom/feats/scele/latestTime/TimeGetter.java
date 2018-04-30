b6bce279031b55953ea24630b2b58481820df94d
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-04-30 18:31:25 +0700

    Bikin berita jd markup kyk login (rikues dennis)

--- src/main/java/com/pacilkom/feats/scele/latestTime/TimeGetter.java
+++ src/main/java/com/pacilkom/feats/scele/latestTime/TimeGetter.java
@@ -1,25 +1,25 @@
 package com.pacilkom.feats.scele.latestTime;
 
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Date;
 
-import org.apache.commons.net.ntp.NTPUDPClient;
-import org.apache.commons.net.ntp.TimeInfo;
-
 
 public class TimeGetter {
 
-    private static final String TIME_SERVER = "ntp.ui.ac.id";  
+    private static final String TIME_SERVER = "ntp.ui.ac.id";
 
-  
-    
+
+
     public static String getTime() {
         return timeLookup();
     }
- 
+
     private static String timeLookup() {
-         
+
         NTPUDPClient timeClient = new NTPUDPClient();
         InetAddress inetAddress;
         try {
@@ -33,6 +33,6 @@ public class TimeGetter {
         }
         return null;
     }
-    
+
 
 }
--- src/test/java/com/pacilkom/feats/scele/latestNews/SceleNewsCommandTest.java
+++ src/test/java/com/pacilkom/feats/scele/latestNews/SceleNewsCommandTest.java
@@ -3,6 +3,8 @@ package com.pacilkom.feats.scele.latestNews;
 import org.junit.Before;
 import org.junit.Test;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,25 +15,27 @@ public class SceleNewsCommandTest {
     private Long chatId = Long.valueOf(001);
     private SendMessage msg;
     private List<Hyperlink> titles;
+    private List<List<InlineKeyboardButton>> news;
 
     @Before
     public void setUp() throws Exception {
         msg = new SceleNewsCommand().execute(chatId);
         titles = NewsScrapper.getNews();
+        news = ((InlineKeyboardMarkup) msg.getReplyMarkup()).getKeyboard();
     }
 
     @Test
     public void hasCorrectNumberOfNews() {
-       String[] news = msg.getText().split("\n\n");
-       assertEquals(titles.size(), news.length);
+       int size = news.size();
+       assertEquals(titles.size(), size);
     }
 
     @Test
     public void checkTheReplyIsCorrect() {
-        String[] news = msg.getText().split("\n\n");
         int pointer = 0;
-        for (Hyperlink title : titles) {
-            assertTrue(news[pointer].equals(title.toString()));
+        for (List<InlineKeyboardButton> bubble : news) {
+            assertTrue(titles.get(pointer).getContent().equals(bubble.get(0).getText()));
+            assertTrue(titles.get(pointer).getLink().equals(bubble.get(0).getUrl()));
             pointer++;
         }
     }
