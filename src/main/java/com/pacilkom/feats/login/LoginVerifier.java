57c948dc7535d5e705c1db1961bf3aae48d7f0a5
Imbang Murtito <imbangmurtito@gmail.com>
2018-05-05 15:41:02 +0700

    Create test for login package and create CSUIAccount

--- src/main/java/com/pacilkom/feats/login/LoginVerifier.java
+++ src/main/java/com/pacilkom/feats/login/LoginVerifier.java
@@ -1,15 +1,8 @@
 package com.pacilkom.feats.login;
 
-import com.pacilkom.bot.PacilkomBot;
+import com.pacilkom.csui.CSUIAccount;
 import com.pacilkom.csuilogin.DatabaseController;
-import com.pacilkom.csuilogin.SessionDatabase;
-import org.json.JSONObject;
 
-import javax.xml.crypto.Data;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Map;
 
 public class LoginVerifier {
@@ -18,21 +11,7 @@ public class LoginVerifier {
         String result = "";
         try {
             String accessToken = DatabaseController.getAccessToken(userId);
-            URL url = new URL("https://akun.cs.ui.ac.id/oauth/token/verify/?access_token="
-                    + accessToken + "&client_id=" + PacilkomBot.CSUI_CLIENT_ID + "&format=json");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-            String line;
-            while ((line = rd.readLine()) != null) {
-                result += line;
-            }
-
-            rd.close();
-            Map<String, Object> loginData = new JSONObject(result).toMap();
-            loginData.put("access_token", accessToken);
-            return loginData;
+            return CSUIAccount.verifyAccessToken(accessToken);
         } catch (Exception e) {
             return null;
         }
