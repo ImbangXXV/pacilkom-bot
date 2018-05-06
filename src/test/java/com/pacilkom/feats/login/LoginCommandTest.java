5d307d5d8f785e6634ab937c2a8d2add67b97ee3
Ichlasul Affan <ichlaffterlalu@gmail.com>
2018-05-06 09:04:03 +0700

    Hotfix: fixed login test to not revealing personal identity

--- src/test/java/com/pacilkom/feats/login/LoginCommandTest.java
+++ src/test/java/com/pacilkom/feats/login/LoginCommandTest.java
@@ -2,6 +2,7 @@ package com.pacilkom.feats.login;
 
 import com.pacilkom.csui.CSUIAccount;
 import com.pacilkom.csuilogin.DatabaseController;
+import com.pacilkom.csuilogin.Encryptor;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +18,10 @@ public class LoginCommandTest {
 
     @Before
     public void setUp() throws Exception{
-        String accessToken = CSUIAccount.getAccessToken("muhammad.imbang", "aliceinwonderland25");
+        String username = Encryptor.decrypt("cf437be0b0085201b64a01ebdc102196");
+        String password = Encryptor.decrypt("6b6157a375f9a5593198e835497617173421b904a8b3"
+                + "10541601ab14f8d1ce38");
+        String accessToken = CSUIAccount.getAccessToken(username, password);
         DatabaseController.createSession(-1, accessToken);
         DatabaseController.createSession(-2, "1234");
         testCommand = new LoginCommand();
@@ -26,14 +30,15 @@ public class LoginCommandTest {
     @Test
     public void botReplyWhenUserStillInSession() {
         SendMessage message = testCommand.execute((long) 1,-1);
-        assertEquals(message.getText(), "Kamu sudah login dengan akun CSUI: muhammad.imbang dengan role mahasiswa");
+        assertEquals(message.getText(), "You have logged in via CSUI account as: muhammad.imbang with role mahasiswa");
     }
 
     @Test
     public void botReplyWhenUserNotInSession() {
         SendMessage message = testCommand.execute((long) 1, -2);
         assertEquals(message.getText(),
-                "Kamu belum login dengan akun CSUI. Silahkan login melalui tombol login berikut!");
+                "You haven't logged in via CSUI account yet. Use this button below to "
+                                                    + "unlock CSUI-exclusive features.");
     }
 
     @After
